c10c717c83fc636d815744ab71ca00ccb4e8a0a9
georgecode <georgeczurlanis@gmail.com>
2014-11-03 19:41:23 -0500

    EZ Keyboard v0.01
    Only 5 notes and only  Em pentatonic

---
+++ KeyBoard/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){ 	        
+        $('#d').click(function(){
+            $('#d').html('<audio autoplay> <source src="sounds/d_note.mp3" type="audio/mpeg" id="aNote"></audio>')   
+        });
+        $('#e').click(function(){
+            $('#e').html('<audio autoplay> <source src="sounds/e_note.mp3" type="audio/mpeg" id="aNote"></audio>')   
+        });
+        $('#g').click(function(){
+            $('#g').html('<audio autoplay> <source src="sounds/g_note.mp3" type="audio/mpeg" id="aNote"></audio>')   
+        });
+        $('#a').click(function(){
+            $('#a').html('<audio autoplay> <source src="sounds/a_note.mp3" type="audio/mpeg" id="aNote"></audio>')   
+        });
+        $('#b').click(function(){
+            $('#b').html('<audio autoplay> <source src="sounds/b_note.mp3" type="audio/mpeg" id="aNote"></audio>')   
+        });
+});
+
+
+
+ //$('#aNote').play();
+
+  //$(this).fadeOut('fast');
