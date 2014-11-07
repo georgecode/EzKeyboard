24c827ca2c093da73d1e8e29bdaa001f518d0aa7
georgecode <georgeczurlanis@gmail.com>
2014-11-07 01:51:56 -0500

    EZ Keyboard v0.02
    Added new library of mp3's now including two octaves of every note,
    reset all the id's to more revenant names, updated the whatKey function
    to include 9 notes allowing for major's and relative minors, updated
    all instances where # was used to indicate a sharp note and used x in
    place of #(because the # symbol kept breaking the code), created a
    sound div so all sound were assigned to the same div, this prevented
    them from ringing out if another key was engaged

--- KeyBoard/script.js
+++ KeyBoard/script.js
@@ -1,71 +1,116 @@
 $(document).ready(function(){ 
-    var cMp3 = '<audio autoplay> <source src="sounds/c_note.mp3" type="audio/mpeg"></audio>'
-    var dMp3 = '<audio autoplay> <source src="sounds/d_note.mp3" type="audio/mpeg"></audio>'
-    var eMp3 = '<audio autoplay> <source src="sounds/e_note.mp3" type="audio/mpeg"></audio>'
-    var fMp3 = '<audio autoplay> <source src="sounds/f_note.mp3" type="audio/mpeg"></audio>'
-    var gMp3 = '<audio autoplay> <source src="sounds/g_note.mp3" type="audio/mpeg"></audio>'
-    var aMp3 = '<audio autoplay> <source src="sounds/a_note.mp3" type="audio/mpeg"></audio>'
-    var bMp3 = '<audio autoplay> <source src="sounds/b_note.mp3" type="audio/mpeg"></audio>'
 
 
 
-        $('#c').click(function(){
-            $('#c').html(cMp3)   
+    var c3mp3 = '<audio autoplay> <source src="sounds/notes/C3.mp3" type="audio/mpeg"></audio>'
+    var cx3mp3 = '<audio autoplay> <source src="sounds/notes/Cx3.mp3" type="audio/mpeg"></audio>'
+
+    var d3Mp3 = '<audio autoplay> <source src="sounds/notes/D3.mp3" type="audio/mpeg"></audio>'
+
+    var dx3Mp3 = '<audio autoplay> <source src="sounds/notes/Dx3.mp3" type="audio/mpeg"></audio>'
+
+    var e3Mp3 = '<audio autoplay> <source src="sounds/notes/E3.mp3" type="audio/mpeg"></audio>'
+    var f3Mp3 = '<audio autoplay> <source src="sounds/notes/F3.mp3" type="audio/mpeg"></audio>'
+    var fx3Mp3 = '<audio autoplay> <source src="sounds/notes/Fx3.mp3" type="audio/mpeg"></audio>'
+    var g3Mp3 = '<audio autoplay> <source src="sounds/notes/G3.mp3" type="audio/mpeg"></audio>'
+    var gx3Mp3 = '<audio autoplay> <source src="sounds/notes/Gx3.mp3" type="audio/mpeg"></audio>'
+    var a3Mp3 = '<audio autoplay> <source src="sounds/notes/A3.mp3" type="audio/mpeg"></audio>'
+    var ax3Mp3 = '<audio autoplay> <source src="sounds/notes/Ax3.mp3" type="audio/mpeg"></audio>'
+    var b3Mp3 = '<audio autoplay> <source src="sounds/notes/B3.mp3" type="audio/mpeg"></audio>'
+    
+    var c4mp3 = '<audio autoplay> <source src="sounds/notes/C4.mp3" type="audio/mpeg"></audio>'
+    var cx4mp3 = '<audio autoplay> <source src="sounds/notes/Cx4.mp3" type="audio/mpeg"></audio>'
+    var d4Mp3 = '<audio autoplay> <source src="sounds/notes/D4.mp3" type="audio/mpeg"></audio>'
+    var dx4Mp3 = '<audio autoplay> <source src="sounds/notes/Dx4.mp3" type="audio/mpeg"></audio>'
+    var e4Mp3 = '<audio autoplay> <source src="sounds/notes/E4.mp3" type="audio/mpeg"></audio>'
+    var f4Mp3 = '<audio autoplay> <source src="sounds/notes/F4.mp3" type="audio/mpeg"></audio>'
+    var fx4Mp3 = '<audio autoplay> <source src="sounds/notes/Fx4.mp3" type="audio/mpeg"></audio>'
+    var g4Mp3 = '<audio autoplay> <source src="sounds/notes/G4.mp3" type="audio/mpeg"></audio>'
+    var gx4Mp3 = '<audio autoplay> <source src="sounds/notes/Gx4.mp3" type="audio/mpeg"></audio>'
+    var a4Mp3 = '<audio autoplay> <source src="sounds/notes/A4.mp3" type="audio/mpeg"></audio>'
+    var ax4Mp3 = '<audio autoplay> <source src="sounds/notes/Ax4.mp3" type="audio/mpeg"></audio>'
+    var b4Mp3 = '<audio autoplay> <source src="sounds/notes/B4.mp3" type="audio/mpeg"></audio>'
+    
+
+
+
+        $('#key1').click(function(){
+            $('#key1').html(c3Mp3)
+       
         });
-        $('#d').click(function(){
-            $('#d').html(dMp3)   
+        $('#key2').click(function(){
+            $('#key2').html(d3Mp3)   
         });
-        $('#e').click(function(){
-               $('#e').html(eMp3)
+        $('#key3').click(function(){
+               $('#key3').html(e3Mp3)
         });
-        $('#f').click(function(){
-            $('#f').html(fMp3)   
+        $('#key4').click(function(){
+            $('#key4').html(f3Mp3)   
         });
-        $('#g').click(function(){
-            $('#g').html(gMp3)   
+        $('#key5').click(function(){
+            $('#key5').html(g3Mp3)   
         });
-        $('#a').click(function(){
-            $('#a').html(aMp3)   
+        $('#key6').click(function(){
+            $('#key6').html(a3Mp3)   
         });
-        $('#b').click(function(){
-            $('#b').html(bMp3)   
+        $('#key7').click(function(){
+            $('#key7').html(b3Mp3)   
+        });
+        $('#key8').click(function(){
+            $('#key8').html(b3Mp3)   
+        });
+        $('#key9').click(function(){
+            $('#key9').html(b3Mp3)   
         });
 
 
 
 //------------whatKey function start
-    function whatKey(one,two,three,four,five){
+    function whatKey(one,two,three,four,five,six,seven,eight,nine){
         $(document).keydown(function(key){
             switch(parseInt(key.which)){
                 case 65:
-                    $('#d').html(one);
+                    $('#sound').html(one);
                 break;
                 case 83:
-                    $('#e').html(two);
+                    $('#sound').html(two);
                 break;
                 case 68:
-                    $('#g').html(three);
+                    $('#sound').html(three);
                 break;
                 case 70:
-                    $('#a').html(four);
+                    $('#sound').html(four);
                 break;
                 case 71:
-                    $('#b').html(five);
+                    $('#sound').html(five);
+                break;
+                case 72:
+                    $('#sound').html(six);
+                break;
+                case 74:
+                    $('#sound').html(seven);
+                break;
+                case 75:
+                    $('#sound').html(eight);
+                break;
+                case 76:
+                    $('#sound').html(nine);
                 break;
             }
         });
     } 
 //------------whatKey function end
     $('#em1').click(function(){
-        var eMinor = whatKey(dMp3,eMp3,gMp3,aMp3,bMp3) 
+        var eMinor = whatKey(e3Mp3,g3Mp3,a3Mp3,b3Mp3,d4Mp3,e4Mp3,g4Mp3,a4Mp3,b4Mp3) 
     });
         
     $('#c1').click(function(){
-        var cMajor = whatKey(cMp3,dMp3,eMp3,gMp3,aMp3)
+        var cMajor = whatKey(c3Mp3,d3Mp3,e3Mp3,g3Mp3,a3Mp3)
     });
 
 
-//----------document ready end    
+//----------document ready end
+    
 });
 
 
